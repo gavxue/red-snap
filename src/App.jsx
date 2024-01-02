e82b920290eed3c614cc2de02236cabe16167424
Gavin Xue <gavinxue050705@gmail.com>
2024-01-02 14:05:47 -0500

    Added pic descriptino from api

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Image from "./components/Image";
 function App() {
   const [data, setData] = useState([]);
   const url =
-    "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=0InkMC1nDfn2Bt7aZm0ZNKH7rjoAUiMSg7YKwh1q";
+    "https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=0InkMC1nDfn2Bt7aZm0ZNKH7rjoAUiMSg7YKwh1q";
 
   useEffect(() => {
     fetch(url)
@@ -20,22 +20,6 @@ function App() {
         console.log(error);
       });
   }, []);
-
-  // const img = document.querySelector("img");
-  // fetch(
-  //   "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=0InkMC1nDfn2Bt7aZm0ZNKH7rjoAUiMSg7YKwh1q",
-  //   { mode: "cors" }
-  // )
-  //   .then((response) => {
-  //     return response.json();
-  //   })
-  //   .then((response) => {
-  //     img.src = response.latest_photos[1].img_src;
-  //     console.log(response);
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
 
   return (
     <div>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -5,15 +5,16 @@ function Image({ data }) {
   if (data.length === 0) {
     return <p>Loading...</p>;
   }
+  console.log(data);
+  const photo = data[335];
 
   return (
     <div>
-      <img src={data[0].img_src} alt="" />
-      {/* {data &&
-        data.map((photo, i) => {
-          console.log(photo);
-          //   <img src={photo.img_src} alt={i} />;
-        })} */}
+      <img src={photo.img_src} alt="" />
+      <p>Rover: {photo.rover.name}</p>
+      <p>Camera: {photo.camera.full_name}</p>
+      <p>Sol: {photo.sol}</p>
+      <p>Earth date: {photo.earth_date}</p>
     </div>
   );
 }
